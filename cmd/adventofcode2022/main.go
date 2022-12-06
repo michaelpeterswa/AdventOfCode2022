010156e2e76d0b9836874adf0b4e0d9834bddc67
Michael Peters <michaelpeterswa@icloud.com>
2022-12-06 10:43:33 -0800

    fix: add day 6

--- cmd/adventofcode2022/main.go
+++ cmd/adventofcode2022/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "github.com/michaelpeterswa/AdventOfCode2022/internal/day5"
+import (
+	"github.com/michaelpeterswa/AdventOfCode2022/internal/day6"
+)
 
 func main() {
 	// day1.Day1("internal/day1/day1.txt")
 	// day2.Day2("internal/day2/day2.txt")
 	// day3.Day3("internal/day3/day3.txt")
 	// day4.Day4("internal/day4/day4.txt")
-	day5.Day5("internal/day5/day5.txt")
+	// day5.Day5("internal/day5/day5.txt")
+	day6.Day6("internal/day6/day6.txt")
 }
--- internal/day6/day6.go
+++ internal/day6/day6.go
@@ -17,17 +17,62 @@
 package day6
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
-func day6(filename string){
+func Day6(filename string) {
 	fmt.Printf("AoC day6 2022\n\n")
 
-	data, err := os.ReadFile(filename)
+	file, err := os.Open(filename)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(data))
+	var input []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		input = append(input, scanner.Text())
+	}
+
+	// Part 1
+	part1(input)
+
+	// Part 2
+	part2(input)
+}
+
+func part1(input []string) {
+	for _, line := range input {
+		for i := 4; i < len(line); i++ {
+			if !containsDuplicate(line[i-4 : i]) {
+				fmt.Println(i)
+				break
+			}
+		}
+	}
+}
+
+func part2(input []string) {
+	for _, line := range input {
+		for i := 14; i < len(line); i++ {
+			if !containsDuplicate(line[i-14 : i]) {
+				fmt.Println(i)
+				break
+			}
+		}
+	}
+}
+
+func containsDuplicate(s string) bool {
+	var runeMap = make(map[rune]bool)
+	for _, c := range s {
+		if _, ok := runeMap[c]; !ok {
+			runeMap[c] = true
+		} else {
+			return true
+		}
+	}
+	return false
 }
